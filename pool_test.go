0bf7956820e89c83b09f712f4ad4680af6131019
Ivan Lutokhin <ivan.lutokhin@gmail.com>
2021-11-25 10:32:47 +0300

    [pool] Refactoring of structure

--- pool_test.go
+++ pool_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 )
 
-func newMockPool(min, max int) (*Pool, error) {
-	return NewPool(min, max, func() (*Client, error) { return NewClient(NewMockConn(nil, nil)), nil })
+func newMockPool(capacity int, open bool) (*Pool, error) {
+	return NewPool(func() (*Client, error) { return NewClient(NewMockConn(nil, nil)), nil }, capacity, open)
 }
 
 func TestPool_Get(t *testing.T) {
-	p, err := newMockPool(1, 2)
+	p, err := newMockPool(1, true)
 	if err != nil {
 		t.Error(err)
 	}
@@ -18,10 +18,14 @@ func TestPool_Get(t *testing.T) {
 		t.Fatal("expected 1, but got", p.Len())
 	}
 
+	if err = p.Open(); err == nil {
+		t.Fatal("expected error, but got nil")
+	}
+
 	// get exists client from pool
 	client1, err := p.Get()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if client1 == nil {
@@ -35,7 +39,7 @@ func TestPool_Get(t *testing.T) {
 	// use factory to create new client
 	client2, err := p.Get()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if client2 == nil {
@@ -45,10 +49,18 @@ func TestPool_Get(t *testing.T) {
 	if p.Len() != 0 {
 		t.Fatal("expected 0, but got", p.Len())
 	}
+
+	if err = p.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = p.Get(); err == nil {
+		t.Fatal("expected error, but got nil")
+	}
 }
 
 func TestPool_Put(t *testing.T) {
-	p, err := newMockPool(0, 1)
+	p, err := newMockPool(1, false)
 	if err != nil {
 		t.Error(err)
 	}
@@ -57,10 +69,14 @@ func TestPool_Put(t *testing.T) {
 		t.Fatal("expected 0, but got", p.Len())
 	}
 
-	// use factory to create new client
+	if err = p.Open(); err != nil {
+		t.Fatal(err)
+	}
+
+	// get exists client from pool
 	client1, err := p.Get()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if client1 == nil {
@@ -74,7 +90,7 @@ func TestPool_Put(t *testing.T) {
 	// use factory to create new client
 	client2, err := p.Get()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if client2 == nil {
@@ -87,7 +103,7 @@ func TestPool_Put(t *testing.T) {
 
 	// put to pool
 	if err = p.Put(client1); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if p.Len() != 1 {
@@ -96,10 +112,18 @@ func TestPool_Put(t *testing.T) {
 
 	// put to pool and close client (max limit)
 	if err = p.Put(client2); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if p.Len() != 1 {
 		t.Fatal("expected 1, but got", p.Len())
+	}
+
+	if err = p.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if err = p.Put(nil); err == nil {
+		t.Fatal("expected error, but got nil")
 	}
 }
